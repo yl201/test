980bed402e83a8e5f539b114c1d5eb4bfef370e6
unknown <yl201@rice.edu>
2021-08-26 17:48:27 -0500

    first version of my code

---
+++ first.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+    /**
+     * Entry point into program.
+     * @param args  The command line arguments.
+     */
+    public static void main(String[] args) {
+	System.out.println("hello yuhan, calm down")
+
+    }
